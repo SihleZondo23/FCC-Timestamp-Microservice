9a08df72c9a2f3106dd403a6b72f4d937fedb69f
Replit user <>
2023-09-14 16:59:01 +0000

    Everything

--- index.js
+++ index.js
@@ -18,13 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
+
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+  
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    resturn;
+  }
+  
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
+app.get("/api", (req, res) =>{
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
